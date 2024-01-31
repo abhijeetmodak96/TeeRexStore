dd047a8b17aad6ec581fe1c13021c8136707288d
Abhijeet Modak <abhijeetmodak96@gmail.com>
2024-01-31 19:56:52 +0530

    project setuo and header

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+//import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
+      <div className="logo-container">
+        <img
+          className="w-48 px-3 pt-2"
+          alt="logoImage"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg3On1qCvY8W7kSirLlluMS9pC9c78SZeNLw&usqp=CAU"
+        />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-2 m-3">
+          <li className="px-3">Home</li>
+          <li className="px-3">Products</li>
+          <li className="px-3 ">Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="backdrop-filter">
+      <ul className="backdrop-filter">
+        color
+        <li className="backdrop-filter">red</li>
+        <li>blue</li>
+        <li>green</li>
+        <li>orange</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
